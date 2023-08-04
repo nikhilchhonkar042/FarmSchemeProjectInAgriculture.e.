a2ca1de37ec219ddf7b1b75c3df890bb77b193c2
Vishal Mate <94169726+vishalmateq@users.noreply.github.com>
2023-08-04 08:52:50 +0530

    Update InsuranceClaimDao.java

--- Vishal/ZFarmerProject/src/com/insuranceClaim/dao/InsuranceClaimDao.java
+++ Vishal/ZFarmerProject/src/com/insuranceClaim/dao/InsuranceClaimDao.java
@@ -1,6 +1,15 @@
 //POJI
 package com.insuranceClaim.dao;
 
-public interface InsuranceClaimDao {
+import java.util.List;
 
+public interface InsuranceClaimDao {
+	
+	public void insertInsurance(InsuranceClaimEntities insurance); //C
+	
+	public InsuranceClaimEntities selectInsurance(int policyNo);//R
+	public List<InsuranceClaimEntities> selectInsurance(); //RA
+	
+	public void updateInsurance(InsuranceClaimEntities insurance); //U
+	public void deleteInsurance(int policyNo);  //D
 }
