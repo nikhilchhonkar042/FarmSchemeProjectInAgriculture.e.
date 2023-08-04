1d81b6e942a5fac2edac01c97e7ff9501e429e44
Vishal Mate <94169726+vishalmateq@users.noreply.github.com>
2023-08-04 08:53:31 +0530

    Update InsuranceClaimDaoImplementation.java

--- Vishal/ZFarmerProject/src/com/insuranceClaim/dao/InsuranceClaimDaoImplementation.java
+++ Vishal/ZFarmerProject/src/com/insuranceClaim/dao/InsuranceClaimDaoImplementation.java
@@ -1,5 +1,42 @@
-package com.insuranceClaim.dao;
+ 
 //POJO implementation
-public class InsuranceClaimDaoImplementation {
+package com.insuranceClaim.dao;
 
+import java.util.List;
+
+public class InsuranceClaimDaoImplementation implements InsuranceClaimDao{
+
+	
+	
+	@Override
+	public void insertInsurance(InsuranceClaimEntities insurance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public InsuranceClaimEntities selectInsurance(int policyNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<InsuranceClaimEntities> selectInsurance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateInsurance(InsuranceClaimEntities insurance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteInsurance(int policyNo) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
+
